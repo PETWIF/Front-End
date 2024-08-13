ccdb2db571c7e32e6786296f85bcef1b9ccc8def
haesa <xdhaesa@gmail.com>
2024-08-13 15:06:42 +0900

    #30 [FEAT] 차단 계정 목록 페이지 UI

--- src/pages/SettingPage/BlockListPage/BlockListPage.style.jsx
+++ src/pages/SettingPage/BlockListPage/BlockListPage.style.jsx
@@ -1,1 +1,69 @@
 import styled from 'styled-components';
+
+import { Layout } from '../../../components/Common';
+
+export const SettingLayout = styled(Layout)`
+  height: 86vh;
+  padding: 49px 52px;
+`;
+
+export const Content = styled.div`
+  width: 75%;
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 8px;
+  border-bottom: 1px solid #dfdfdf;
+  font-weight: 500;
+  font-size: 23px;
+  line-height: 33px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Fieldset = styled.div`
+  margin-bottom: 38px;
+`;
+
+export const Field = styled.div`
+  margin: 16px 0;
+`;
+
+export const Label = styled.div`
+  margin-bottom: 6px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+  color: #1e1e1e;
+`;
+
+export const Text = styled.p`
+  margin: 11px 0;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 23px;
+`;
+
+export const FriendList = styled.ul`
+  width: 50%;
+  margin: 23px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+
+export const FriendItem = styled.li`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 23px;
+  color: ${(props) => props.theme.color.black};
+
+  & > div {
+    display: flex;
+    align-items: center;
+    gap: 13px;
+  }
+`;
--- src/pages/SettingPage/VisibilityPreferencePage/VisibilityPreferencePage.jsx
+++ src/pages/SettingPage/VisibilityPreferencePage/VisibilityPreferencePage.jsx
@@ -1,11 +1,5 @@
 import { Button } from '../../../components/Button';
-import {
-  Checkbox,
-  Dropdown,
-  Radio,
-  RadioGroup,
-  TextInput,
-} from '../../../components/Input';
+import { Radio, RadioGroup } from '../../../components/Input';
 
 import * as S from './VisibilityPreferencePage.style.jsx';
 
@@ -19,7 +13,7 @@ export default function VisibilityPreferencePage() {
             <S.Label>공개 여부</S.Label>
             <S.Text>
               비공개 설정 시 친구들만 사용자의 앨범 또는 친구 목록을 확인할 수
-              있습니다
+              있습니다.
             </S.Text>
             <RadioGroup
               name='visibility'
