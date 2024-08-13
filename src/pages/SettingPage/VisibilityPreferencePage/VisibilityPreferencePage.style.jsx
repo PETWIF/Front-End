f5071f078b6737964114e779a29c5781ac948aba
haesa <xdhaesa@gmail.com>
2024-08-13 14:58:20 +0900

    #30 [FEAT] 계정 공개 범위 설정 페이지 UI

---
+++ src/pages/SettingPage/VisibilityPreferencePage/VisibilityPreferencePage.style.jsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+import { Layout } from '../../../components/Common';
+
+export const SettingLayout = styled(Layout)`
+  height: 86vh;
+  padding: 49px 52px;
+`;
+
+export const Content = styled.div`
+  width: 75%;
+`;
+
+export const Title = styled.h2`
+  padding-bottom: 8px;
+  border-bottom: 1px solid #dfdfdf;
+  font-weight: 500;
+  font-size: 23px;
+  line-height: 33px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Fieldset = styled.div`
+  margin-bottom: 38px;
+`;
+
+export const Field = styled.div`
+  margin: 16px 0;
+`;
+
+export const Label = styled.div`
+  margin-bottom: 6px;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 140%;
+  color: #1e1e1e;
+`;
+
+export const Text = styled.p`
+  margin: 11px 0;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 23px;
+`;
