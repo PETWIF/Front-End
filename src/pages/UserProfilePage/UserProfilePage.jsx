b02593956836b494af881032483255c139900505
doirang1 <doirang1@gmail.com>
2024-09-20 02:10:20 +0900

    [FEAT] 프로필 수정 api 연결

--- src/pages/UserProfilePage/UserProfilePage.jsx
+++ src/pages/UserProfilePage/UserProfilePage.jsx
@@ -1,102 +1,215 @@
+import { useState } from 'react';
+
+import { useAuth } from '../../hooks/useAuth.jsx';
+
+import { patchAddUserInfo, postAddPetInfo } from '../../apis/addInfo.js';
+
+import useModal from '../../hooks/useModal.jsx';
+
 import { Button } from '../../components/Button';
-import {
-  Checkbox,
-  Dropdown,
-  Radio,
-  RadioGroup,
-  TextInput,
-} from '../../components/Input';
-import { Flex } from '../../components/Common';
 import { Profile } from '../../components/Profile';
 import { RandomFriend } from '../../components/RandomFriend';
 
-import { AGE_LIST, TELECOM_LIST } from '../../constants';
-
 import * as S from './UserProfilePage.style.jsx';
 
-const userId = 1;
-
 export default function UserProfilePage() {
+  const { userId } = useAuth();
+  const { isOpen, open, close, Modal } = useModal();
+
+  const [gender, setGender] = useState('');
+  const [year, setYear] = useState(''); 
+  const [month, setMonth] = useState('');
+  const [day, setDay] = useState('');
+  const [telecom, setTelecom] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [petName, setPetName] = useState('');
+  const [petGender, setPetGender] = useState('');
+  const [petAge, setPetAge] = useState('');
+  const [petKind, setPetKind] = useState('');
+
+  const birthDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log({ gender, birthDate, telecom, phone, address });
+    console.log({ petName, petGender, petAge, petKind });
+
+    try {
+      let userRes, petRes;
+
+      [userRes, petRes] = await Promise.all([
+        patchAddUserInfo({ gender, birthDate, telecom, phone, address }),
+        postAddPetInfo({ petName, petGender, petAge, petKind })
+      ]);
+
+      console.log(petRes, userRes);
+      const { isSuccess } = petRes;
+
+      if (isSuccess) {
+        console.log('정보 입력 성공');
+        open();
+      } else {
+        console.error('펫:', petRes);
+      }
+    } catch (error) {
+      console.error('정보 입력 중 에러 발생:', error);
+    }
+  };
+
   return (
     <S.UserProfileLayout>
       <S.MainWrapper>
         <S.MainContainer>
-          <S.Fieldset>
-            <S.Title>회원정보</S.Title>
-            <S.Field>
-              <S.Label>성별</S.Label>
-              <RadioGroup name='gender' defaultValue='male' callback={() => {}}>
-                <Radio value='male' defaultChecked>
-                  남성
-                </Radio>
-                <Radio value='female'>여성</Radio>
-              </RadioGroup>
-            </S.Field>
-            <S.Field>
-              <S.Label>생년월일</S.Label>
-              <Flex $gap='17px'>
-                <TextInput type='text' placeholder='년도' />
-                <TextInput type='text' placeholder='월' />
-                <TextInput type='text' placeholder='일' />
-              </Flex>
-            </S.Field>
-            <S.Field>
-              <S.Label>전화번호</S.Label>
-              <Flex $gap='17px'>
-                <Dropdown
-                  width='96px'
-                  options={TELECOM_LIST}
-                  placeholder='통신사'
-                  callback={(data) => {}}
+        <S.FormWrapper>
+            <S.FormContainer>
+              <S.MainBoldText>회원 정보</S.MainBoldText>
+              <S.StyledHr />
+              <S.MainBoldText>성별</S.MainBoldText>
+              <S.InputWrapper>
+                <S.InputStyle
+                  $width='20px'
+                  type='radio'
+                  name='gender'
+                  value='MALE'
+                  onChange={(e) => setGender(e.target.value)}
                 />
-                <TextInput type='text' placeholder='전화번호를 입력해주세요' />
-              </Flex>
-            </S.Field>
-            <S.Field>
-              <S.Label>거주지</S.Label>
-              <Flex $gap='17px'>
-                <TextInput type='text' placeholder='거주지를 입력해주세요' />
-                <Button width='120px' padding='11px'>
-                  주소 검색
+                <S.MainNormalText>남성</S.MainNormalText>
+                <S.InputStyle
+                  $width='20px'
+                  type='radio'
+                  name='gender'
+                  value='FEMALE'
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                <S.MainNormalText>여성</S.MainNormalText>
+              </S.InputWrapper>
+              <S.MainBoldText>생년월일</S.MainBoldText>
+              <S.InputWrapper>
+                <S.InputStyle
+                  $width='170px'
+                  type='text'
+                  className='year'
+                  placeholder='연도(YYYY)'
+                  onChange={(e) => setYear(e.target.value)}
+                />
+                <S.InputStyle
+                  $width='170px'
+                  type='text'
+                  className='month'
+                  placeholder='월(MM)'
+                  onChange={(e) => setMonth(e.target.value)}
+                />
+                <S.InputStyle
+                  $width='170px'
+                  type='text'
+                  className='day'
+                  placeholder='일(DD)'
+                  onChange={(e) => setDay(e.target.value)}
+                />
+              </S.InputWrapper>
+              <S.MainBoldText>전화번호</S.MainBoldText>
+              <S.InputWrapper>
+                <S.SelectStyle 
+                  $width='92px' 
+                  className='telecom'
+                  onChange={(e) => setTelecom(e.target.value)}
+                  >
+                  <option value=''>통신사 선택</option>
+                  <option value='SKT'>SKT</option>
+                  <option value='KT'>KT</option>
+                  <option value='LGUPLUS'>LG U+</option>
+                  <option value='ETC'>기타</option>
+                </S.SelectStyle>
+                <S.InputStyle
+                  type='text'
+                  className='phone'
+                  placeholder='전화번호를 입력해 주세요'
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </S.InputWrapper>
+              <S.MainBoldText>거주지</S.MainBoldText>
+              <S.InputWrapper>
+                <S.InputStyle
+                  $width='700px'
+                  type='text'
+                  className='address'
+                  placeholder='거주지를 입력해 주세요 (예: 서울시 용산구)'
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </S.InputWrapper>
+              <br />
+              <S.MainBoldText>반려동물 정보</S.MainBoldText>
+              <S.StyledHr />
+              <S.InputWrapper>
+                <S.InputContainer>
+                  <S.MainBoldText>이름</S.MainBoldText>
+                  <S.InputStyle
+                    $width='300px'
+                    type='text'
+                    placeholder='반려동물의 이름을 입력해 주세요'
+                    onChange={(e) => setPetName(e.target.value)}
+                  />
+                </S.InputContainer>
+              </S.InputWrapper>
+              <S.MainBoldText>성별</S.MainBoldText>
+              <S.InputWrapper>
+                <S.InputStyle
+                  $width='20px'
+                  type='radio'
+                  name='pet-gender'
+                  value='MALE'
+                  onChange={(e) => setPetGender(e.target.value)}
+                />
+                <S.MainNormalText>수컷</S.MainNormalText>
+                <S.InputStyle
+                  $width='20px'
+                  type='radio'
+                  name='pet-gender'
+                  value='FEMALE'
+                  onChange={(e) => setPetGender(e.target.value)}
+                />
+                <S.MainNormalText>암컷</S.MainNormalText>
+              </S.InputWrapper>
+              <S.InputWrapper>
+                <S.InputContainer>
+                  <S.MainBoldText>나이</S.MainBoldText>
+                  <S.InputStyle
+                    $width='300px'
+                    type='text'
+                    placeholder='반려동물의 나이를 입력해 주세요'
+                    onChange={(e) => setPetAge(e.target.value)}
+                  />
+                </S.InputContainer>
+              </S.InputWrapper>
+              <S.InputWrapper>
+                <S.InputContainer>
+                  <S.MainBoldText>종류</S.MainBoldText>
+                  <S.InputStyle
+                    $width='300px'
+                    type='text'
+                    placeholder='반려동물의 종류를 입력해 주세요'
+                    onChange={(e) => setPetKind(e.target.value)}
+                  />
+                </S.InputContainer>
+              </S.InputWrapper>
+                <Button 
+                    onClick={(e) => handleSubmit(e)} 
+                    width='100%' 
+                    padding='15px' 
+                    buttonStyle='orange'>
+                  정보 변경하기
                 </Button>
-              </Flex>
-            </S.Field>
-          </S.Fieldset>
-          <S.Fieldset>
-            <S.Title>반려동물 정보</S.Title>
-            <S.Field>
-              <S.Label>이름</S.Label>
-              <TextInput
-                type='text'
-                placeholder='반려동물의 이름을 입력해주세요'
-              />
-            </S.Field>
-            <S.Field>
-              <S.Label>성별</S.Label>
-              <RadioGroup name='gender' defaultValue='male' callback={() => {}}>
-                <Radio value='male' defaultChecked>
-                  수컷
-                </Radio>
-                <Radio value='female'>암컷</Radio>
-              </RadioGroup>
-            </S.Field>
-            <S.Field>
-              <S.Label>나이</S.Label>
-              <Dropdown
-                width='96px'
-                options={AGE_LIST}
-                placeholder='0세'
-                callback={(data) => {}}
-              />
-            </S.Field>
-          </S.Fieldset>
-          <Button padding='20px'>저장하기</Button>
+            </S.FormContainer>
+          </S.FormWrapper>
         </S.MainContainer>
       </S.MainWrapper>
       <S.SideContainer>
         <Profile userId={userId} />
         <RandomFriend />
       </S.SideContainer>
+      {isOpen && <Modal text='정보 변경이 완료되었어요!' close={close} />}
     </S.UserProfileLayout>
   );
 }
